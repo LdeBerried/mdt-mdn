f330fe68fd7a641bda04699b3578086ca775220e
LdeBerried <44316922+LdeBerried@users.noreply.github.com>
2020-03-27 18:27:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var baseUrl = "https://drv.tw/~laalpri@gmail.com/gd/curio/";
+var spiderSearch = /\baraña blanca|\barañas blancas|\bwhite spider|\barañas albinas|\baraña albina|\balbino spider|\balbino spiders/gi
+var snakeSearch = /\bserpiente Las Vegas|\bserpientes nevada|\bserpiente casino|\bserpiente carta|\bserpiente cartas|\bserpiente baraja cartas/gi
+                
+
+        $(document).ready(function(){
+            var str = $("#inputBusqueda")
+            str.val("");
+
+            $("#botonBusqueda").click(function(){
+                var str = $("#inputBusqueda").val();
+
+                if(str.match(spiderSearch)){
+                    alert("ARAÑAS");
+                    window.location.href = baseUrl+"serpientesSearch.html";
+                }else if(str.match(snakeSearch)){
+                    alert("SERPIENTES");
+                    window.location.href = "serpientesSearch.html";
+                }
+            });
+
+            $(document).on('keypress',function(e){
+                var str = $("#inputBusqueda").val();
+
+                if(e.which == 13) {
+                    if(str.match(spiderSearch)){
+                        alert("ARAÑAS");
+                        window.location.href = baseUrl+"serpientesSearch.html";
+                    }else if(str.match(snakeSearch)){
+                        alert("SERPIENTES");
+                        window.location.href = baseUrl+"serpientesSearch.html";
+                    }
+                }
+            });
+        });
